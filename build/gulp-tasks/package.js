665914c00c4be16b8fd2abb6fa9743543dff6da2
Ajayi Marvellous <ajayimarvellous777@gmail.com>
2022-01-11 16:00:43 +0100

    most recent commit

---
+++ build/gulp-tasks/package.js
@@ -0,0 +1,17 @@
+// generates a new faker package based on a list of locales
+
+var locale = 'en';
+
+/*
+
+  Here you could override the default directory list of all, and replace it with a list of custom locales like:
+
+  var localeList = ['en', 'en_AU', 'de', 'jp', 'ar'];
+
+*/
+
+// iterate through a list of locales and create a new package contain all these locales
+
+// for each locale, create a package
+
+
